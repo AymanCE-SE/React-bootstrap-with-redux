5b992bfcd8031274a23b56797c07f5e2a4d86a51
ayman samir <ayman_Samir_ahmed@hotmail.com>
2025-07-07 08:26:37 +0300

    serverless issue

--- server/api/server.js
+++ server/api/server.js
@@ -1,13 +1,13 @@
-const jsonServer = require('json-server');
-const cors = require('cors');
+import jsonServer from "json-server";
+import cors from "cors";
 
 const server = jsonServer.create();
-const router = jsonServer.router('api/db.json');
+const router = jsonServer.router("api/db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(cors());
 server.use(middlewares);
-server.use(jsonServer.rewriter({ '/api/*': '/$1' }));
 server.use(router);
 
-module.exports = server;
+// Export the handler for Vercel
+export default server;
