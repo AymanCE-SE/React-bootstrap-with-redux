87c91ca47c100523f6ddc76e07ad5f7037d0ed3f
ayman samir <ayman_Samir_ahmed@hotmail.com>
2025-07-07 09:56:17 +0300

    No further backend changes needed for GET requests Readonly

--- server/api/server.js
+++ server/api/server.js
@@ -1,19 +1,8 @@
-// api/server.js
 const jsonServer = require("json-server");
-const fs = require("fs");
 const path = require("path");
-const os = require("os");
-
-// Source: db.json within deployment bundle
-const src = path.join(__dirname, "db.json");
-// Destination: writable path in serverless environment
-const dst = path.join(os.tmpdir(), "db.json");
-
-// Always copy fresh db.json from read-only bundle into tmp
-fs.copyFileSync(src, dst);
 
 const server = jsonServer.create();
-const router = jsonServer.router(dst);
+const router = jsonServer.router(path.join(__dirname, "../db.json"));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
